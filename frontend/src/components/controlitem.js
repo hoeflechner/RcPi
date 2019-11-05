bbc78fa74b632d70e0c668c6e791c95efffdd84f
Bernhard Hoeflechner <bernhard.hoeflechner@siemens.com>
2019-11-05 10:50:49 +0100

    local update works

--- frontend/src/components/controlitem.js
+++ frontend/src/components/controlitem.js
@@ -4,28 +4,25 @@ import Global from '../global';
 function ControlItem(props){
     console.log("item"+ props.id);
     console.log(Global.Config);
-    const [Name, setName] = useState(Global.Config[props.id].Name)
-    const [Axis, setAxis] = useState(Global.Config[props.id].Axis)
+    const [name, setName] = useState(Global.Config[props.id].Name)
+    const [axis, setAxis] = useState(Global.Config[props.id].Axis)
 
     function handleNameChange(e){
-        //setName(e.target.value);
-        console.log("change name item"+ props.id+" to "+e.target.value);
-        console.log(Global.Config);
-        
-        Global.Config[props.id].Name=e.target.value;
+        setName(e.target.value);
+        // console.log("change name item"+ props.id+" to "+e.target.value);
+        // console.log(Global.Config); 
+        Global.Config[props.id].Name=name;
     }
 
     function handleAxisChange(e){
-        //setAxis(e.target.value);
-        Global.Config[props.id].Axis=e.target.value
+        setAxis(e.target.value);
+        Global.Config[props.id].Axis=axis
     }
-
-   
 
     return (
         <div>
-            <input value={Name} onChange={handleNameChange}/>
-            <input value={Axis} onChange={handleAxisChange}/>
+            <input value={name} onChange={handleNameChange}/>
+            <input value={axis} onChange={handleAxisChange}/>
         </div>
     )
 }
